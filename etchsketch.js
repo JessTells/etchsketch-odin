48b65bb2d4f857979161bb97165c4daa5299e9c8
JessTells <jernestot27@gmail.com>
2023-08-13 12:00:08 -0700

    created html file, css, and etchsketch grid layout in js

---
+++ etchsketch.js
@@ -0,0 +1,18 @@
+const gridContainer = document.getElementById("grid-container");
+
+function createGrid(gridDimension){
+    for(let i = 0; i < gridDimension; ++i){
+        const newGridRow = document.createElement("div");
+        newGridRow.classList.add("grid-row");
+        gridContainer.appendChild(newGridRow);
+
+        for(let j = 0; j < gridDimension; ++j){
+            const newGridSquare = document.createElement("div");
+            newGridSquare.classList.add("grid-square");
+            newGridRow.appendChild(newGridSquare);
+        }
+        
+    }
+}
+
+createGrid(16);
